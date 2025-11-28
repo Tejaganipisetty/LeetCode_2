1c748d512f69d0d0e78044fac7097cd1b46e1261
Tejaganipisetty <159601656+Tejaganipisetty@users.noreply.github.com>
2025-11-28 22:35:10 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (88.70%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fun(self,i,j,dp):
+        if(i<0 or j<0):return 0
+        if(i==0 and j==0):return 1
+        if dp[i][j]!=-1:return dp[i][j]
+        a = self.fun(i-1,j,dp)
+        b = self.fun(i,j-1,dp)
+        dp[i][j]=a+b
+        return dp[i][j]
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp=[[-1]*n for _ in range(m)]
+        return self.fun(m-1,n-1,dp)
+        
